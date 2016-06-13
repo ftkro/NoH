6d78c28affb6e8696690aaaafce2fa187b4384db
Fukuda Takuro <ftakuro@otya.me>
2016-06-13 23:17:12 +0900

    Changed

--- lib/jsont.php
+++ lib/jsont.php
@@ -1,7 +1,8 @@
 <?php
+	require_once __DIR__.'/config.php';
         require_once __DIR__.'/leapinfo.php';
         $jsont = array(
-                "id" => "ntp.jre655.com",
+                "id" => "$domain",
                 "it" => str_replace('_','.',reset(array_flip($_GET))),
                 "st" => round(microtime(true),3),
                 "leap" => $leap,
